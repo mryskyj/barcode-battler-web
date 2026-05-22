8e89cf319db2bb4b3553db3a6e72f97d81a3b105
mrysky <285978858+mryskyj@users.noreply.github.com>
2026-05-22 15:36:03 +0900

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { env } from "node:process";
 
 export default defineConfig({
+  base: env.GITHUB_ACTIONS ? "/barcode-battler-web/" : "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
